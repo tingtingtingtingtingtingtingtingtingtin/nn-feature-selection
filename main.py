4ecfb28978769765d61774fc1390586513e6e009
Pineapple-Soup <adithyaiyer412@gmail.com>
2024-05-16 18:02:34 -0700

    Added utilities

--- main.py
+++ main.py
@@ -8,3 +8,19 @@ class Node():
     def __init__(self, features = set(), score = 0):
         self.features = features
         self.score = score
+
+    def __str__(self):
+        if len(self.features) == 0:
+            return f"Using no features and random evaluation, accuracy is {100*self.score}"
+        elif len(self.features) == 1:
+            return f"Feature {self.features} has accuracy {100*self.score}%"
+        else:
+            return f"Features {self.features} have accuracy {100*self.score}%"
+
+    def __lt__(self, other):
+        return self.score < other.score
+    
+    def __eq__(self, other):
+        return self.score < other.score
+    
+
