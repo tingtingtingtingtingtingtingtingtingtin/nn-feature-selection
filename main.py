4529cdf680e125075d2bf0c48192f28e317426a3
Pineapple-Soup <adithyaiyer412@gmail.com>
2024-05-16 18:01:45 -0700

    Made Node Class

---
+++ main.py
@@ -0,0 +1,10 @@
+import copy
+import heapq
+import random
+
+allFeatures = {1, 2, 3, 4, 5, 6}
+
+class Node():
+    def __init__(self, features = set(), score = 0):
+        self.features = features
+        self.score = score
